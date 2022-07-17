7fceacce423872a4c088c0d64fffa78b7e4695a2
Gian Zamora <gz2016@att.net>
2022-07-16 18:26:47 -0700

    adjusted css theming of homepage component

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
 import { Form } from '../Form/form';
+import '../Homepage/homepage.css';
 
 export function Homepage() {
 	return (
-		<Box className="form-box">
-			<Box className="top-section">
+		<Box className="form-box" sx={{ margin: 4 }}>
+			<Box className="top-section" sx={{ mb: 5 }}>
 				<Typography className="head-title" variant="h2">
 					Password Generator
 				</Typography>
